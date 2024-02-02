408e324fda76631866d5dec158be0ccd4b480ae4
jacob <jacob@vkdata.dk>
2024-02-02 13:39:52 +0100

    [IMP] New test

--- Rentra/rentra_backend/rentra/tests.py
+++ Rentra/rentra_backend/rentra/tests.py
@@ -3,5 +3,18 @@ from .models import Product
 
 class TestProduct(TestCase):
 
+    def setUp(self):
+        Product.objects.create(title='Test Product 1', completed=False)
+        Product.objects.create(title='Test Product 2', completed=False)
+
+    def test_Product_title(self):
+        Product1 = Product.objects.get(title='Test Product 1')
+        Product2 = Product.objects.get(title='Test Product 2')
+        self.assertEqual(Product1.title, 'Test Product 1')
+        self.assertEqual(Product2.title, 'Test Product 2')
+
     def test_Product_completed(self):
-        self.assertTrue(1==1)
+        Product1 = Product.objects.get(title='Test Product 1')
+        Product2 = Product.objects.get(title='Test Product 2')
+        self.assertFalse(Product1.completed)
+        self.assertTrue(Product2.completed)
