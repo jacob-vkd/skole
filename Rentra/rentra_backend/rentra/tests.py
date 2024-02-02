4f89e68dde253c66e847a73529d391a3310cf181
jacob <jacob@vkdata.dk>
2024-02-02 13:46:29 +0100

    [IMP] new test

--- Rentra/rentra_backend/rentra/tests.py
+++ Rentra/rentra_backend/rentra/tests.py
@@ -5,8 +5,8 @@ class TestProduct(TestCase):
 
     def setUp(self):
         cat = Category.objects.create(name='Test Category 1')
-        Product.objects.create(name='Test Product 1', description='test Desc', category_id=cat.id, price_type='hourly', price=100)
-        Product.objects.create(name='Test Product 2', description='test Desc', category_id=cat.id, price_type='hourly', price=100)
+        Product.objects.create(name='Test Product 1', description='test Desc', category_id=cat, price_type='hourly', price=100)
+        Product.objects.create(name='Test Product 2', description='test Desc', category_id=cat, price_type='hourly', price=100)
 
     def test_Product_name(self):
         Product1 = Product.objects.get(title='Test Product 1')
